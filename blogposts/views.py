33f2c52366b52b178135196b2956aec31f6562a1
hannah <hanxtan11@gmail.com>
2020-07-20 10:40:24 -0400

    make it better

---
+++ blogposts/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def blogs(request):
+    return render(request, 'blogs/blogs.html')
+
+def home(request):
+    return render(request,'blogs/home.html')
+
+
+
