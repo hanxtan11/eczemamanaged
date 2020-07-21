850519a08bf1b368078a4a2261a298ed03915329
hannah <hanxtan11@gmail.com>
2020-07-20 22:48:20 -0400

    collectstatic

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,14 +120,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/blogposts/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'blogposts/static')
 # STATICFILES_STORAGE = 'whitenoise.djang.GzipManifestStaticFilesStorage'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'blogposts/static'),
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
