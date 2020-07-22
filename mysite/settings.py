e29f09783fb815d0258edcdb36aab5e98c1e7138
hannah <hanxtan11@gmail.com>
2020-07-21 21:54:05 -0400

    hello

--- mysite/settings.py
+++ mysite/settings.py
@@ -129,8 +129,5 @@ STATICFILES_DIRS = (
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
+# 
 django_heroku.settings(locals())
